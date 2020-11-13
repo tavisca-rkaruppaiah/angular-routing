f252a8585ad2668c08b60091d7d6eb21b6634546
Ragu Balagi Karuppaiah <rkaruppaiah@tavisca.in>
2020-11-13 16:55:31 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes:Route[] = [
+  {path :'' , component: LoginComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
